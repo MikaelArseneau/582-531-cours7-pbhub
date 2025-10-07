a7e61684459903d1474b7acea06f62b7b0991c94
MikaelArseneau <mikael.arseneau@hotmail.ca>
2025-10-07 16:56:29 -0400

    direction touch

--- src/main.cpp
+++ src/main.cpp
@@ -4,37 +4,55 @@
 #define BROCHE_ATOM_FIL_JAUNE 26
 #define BOUTON_ATOM_LIGHT 27
 #define BOUTON_ATOM_BTN 39
+#include <M5_PbHub.h>
+#define KEY_CHANNEL 0
 
-  CRGB keyPixel;
-  CRGB keyPixel2;
+M5_PbHub myPbHub;
+CRGB keyPixel;
+CRGB keyPixel2;
 
-void setup() {
-  
+void setup()
+{
+  Wire.begin();
+  myPbHub.begin();
+
   // Initialiser FastLED pour contrôler le pixel RGB du M5Stack Key Unit
-  FastLED.addLeds< WS2812, BROCHE_ATOM_FIL_JAUNE , GRB >(&keyPixel, 1); 
-  FastLED.addLeds< WS2812, BOUTON_ATOM_LIGHT , GRB >(&keyPixel2, 1); 
-  pinMode( BROCHE_ATOM_FIL_BLANC , INPUT_PULLUP );
-  pinMode( BOUTON_ATOM_BTN , INPUT_PULLUP );
+  // FastLED.addLeds<WS2812, BROCHE_ATOM_FIL_JAUNE, GRB>(&keyPixel, 1);
+  FastLED.addLeds<WS2812, BOUTON_ATOM_LIGHT, GRB>(&keyPixel2, 1);
+  myPbHub.setPixelCount( KEY_CHANNEL ,  1 );
 
+  // pinMode(BROCHE_ATOM_FIL_BLANC, INPUT_PULLUP);
+  pinMode(BOUTON_ATOM_BTN, INPUT_PULLUP);
 }
 
-void loop() {
-  int maLectureKey = digitalRead( BROCHE_ATOM_FIL_BLANC );
+void loop()
+{
+  int value = myPbHub.digitalRead(KEY_CHANNEL);
+
+  int maLectureKey = digitalRead(BROCHE_ATOM_FIL_BLANC);
   int maLectureBouton = digitalRead(BOUTON_ATOM_BTN);
-  if(maLectureKey==0){
-  keyPixel = CRGB(255,255,0); // BLANC
-  FastLED.show();
-  }else{
-    keyPixel = CRGB(0,0,0); // BLANC
-    FastLED.show();
+   if (value == 0)
+  {
+    myPbHub.setPixelColor(KEY_CHANNEL, 0, 127,255, 0);
+    
+   }
+   else
+   {
+    myPbHub.setPixelColor(KEY_CHANNEL, 0, 0,0, 0);
+     
   }
-  if(maLectureBouton==0){
-    keyPixel2 = CRGB(255,255,0); // BLANC
-    FastLED.show();
-    }else{
-      keyPixel2 = CRGB(0,0,0); // BLANC
-      FastLED.show();
-    }
-delay(100);
-}
 
+
+  if (maLectureBouton == 0)
+  {
+    keyPixel2 = CRGB(255, 255, 0); // BLANC
+    
+  }
+  else
+  {
+    keyPixel2 = CRGB(0, 0, 0); // BLANC
+   
+  }
+  FastLED.show();
+  delay(100);
+}
